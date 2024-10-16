82104f0637184a72b3cfc90f5faa0a0f772682b9
lays <vaseto.stoianov.94@gmail.com>
2024-10-17 00:44:09 +0300

    Added Json array and json object segmentation fault to be fixed

--- main.c
+++ main.c
@@ -1,23 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 
-// iterate if " start of string " end of string
-struct map {};
+typedef struct {
+  char *value;
+  char *key;
+  int valid;
+} JsonObject;
 
-struct map parse_json(char *json);
+typedef struct {
+  JsonObject jsonObjects[16];
+  unsigned int size;
+} JsonArray;
+
+unsigned long calculate_hash(char *key);
+unsigned int put_in_JsonObject(JsonArray *array, JsonObject jsonObject);
+JsonObject *getObject(JsonArray *array, char *key);
+
+JsonArray parse_json(char *json, JsonArray *array);
 
 int main(void) {
-  char *json = "{\"test\":5}";
+  char *json = "{\"test\":5,\"string\":\"str\"}";
+  JsonArray arr;
+  parse_json(json, &arr);
+  printf("Key in the array is %s \n", getObject(&arr, "test")->key);
   return 1;
 }
 
-struct map parse_json(char *json) {
+unsigned long calculate_hash(char *key) {
+  unsigned long hash = 5351;
+  int c;
+  while (c = *key++) {
+    c = ((hash << 5) + hash) + c;
+  }
+
+  return hash;
+}
+
+unsigned int put_in_JsonObject(JsonArray *array, JsonObject jsonObject) {
+  unsigned int index = calculate_hash(jsonObject.key) & array->size;
+  if (array->jsonObjects[index].valid == 1) {
+    printf("ERROR: The cell its not empty!\n");
+  } else {
+    array->jsonObjects[index] = jsonObject;
+    array->size++;
+    return 1;
+  }
+}
+
+JsonObject *getObject(JsonArray *array, char *key) {
+  int index = calculate_hash(key) & array->size;
+  JsonObject *result = &array->jsonObjects[index];
+
+  if (result->valid == 0) {
+    printf("ERROR: No such object, suck ass!\n");
+    return result;
+  } else {
+    return result;
+  }
+}
+
+JsonArray parse_json(char *json, JsonArray *array) {
   char *value;
   int starting = 0;
   int ending = 0;
   int startOrEnd = 1;
   for (int x = 0; x < sizeof(json); x++) {
-    printf("%c  start: %d , end:%d \n", json[x], starting, ending);
+    // printf("%c  start: %d , end:%d \n", json[x], starting, ending);
+    JsonObject temp = {0};
     if (json[x] == '\"') {
       if (startOrEnd == 1) {
         starting = x;
@@ -28,12 +77,14 @@ struct map parse_json(char *json) {
       }
     }
     if (json[x] == ':') {
-      printf("we are getting the key\n");
       int i = 0;
-      char *key[ending];
+      char key[ending];
       memcpy(key, &json[starting], ending);
-      key[ending - 1] = "/0";
-      printf("key is %s \n", &key);
+      strncpy(temp.key, key, sizeof(temp.key) - 1);
+      printf("the key is %s\n", key);
+      temp.valid = 1;
+      printf("Putting in array with key %s\n", temp.key);
+      put_in_JsonObject(array, temp);
     }
   }
 }

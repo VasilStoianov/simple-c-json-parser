0d0735d843b9429339878161461d88ce61d27b6b
lays <vaseto.stoianov.94@gmail.com>
2024-10-21 23:24:34 +0300

    Added support for number

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+  JSON_VALUE_STRING,
+  JSON_VALUE_INT,
+  JSON_VALUE_BOOLEAN,
+  JSON_VALUE_OBJECT,
+  JSON_VALUE_ARRAY
+} JsonValueType;
+
 typedef struct {
-  char value[50];
-  char key[50];
+  char *key;
+  JsonValueType valueType;
+  union {
+    int intValue;
+    char *stringValue;
+    bool boolValue;
+  } value;
   int valid;
 } JsonObject;
 
@@ -15,18 +29,20 @@ typedef struct {
 } JsonArray;
 
 unsigned int calculate_hash(char *key);
-unsigned int put_in_JsonObject(JsonArray *array, char *key, char *value);
+unsigned int put_in_JsonObject(JsonArray *array, char *key, char *value,
+                               JsonValueType type);
 JsonObject *getObject(JsonArray *array, char *key);
 
 JsonArray parse_json(char *json, JsonArray *array);
 
 int main(void) {
-  char *json = "{\"test\":\"str\",\"string\":\"str\"}";
+  char *json = "{\"test\":\"str\",\"string\":\"str\",\"intvalue\":5}";
   JsonArray arr;
   arr.size = 16;
   parse_json(json, &arr);
   JsonObject *obj = getObject(&arr, "test");
   JsonObject *secObj = getObject(&arr, "string");
+  JsonObject *thirdObj = getObject(&arr, "intvalue");
   if (obj != NULL) {
 
     printf("Object with key %s in the array and value %s  \n", obj->key,
@@ -36,6 +52,10 @@ int main(void) {
 
     printf("Second Object with key %s in the array and value %s  \n",
            secObj->key, secObj->value);
+  }
+  if (thirdObj != NULL) {
+    printf("Third int object key %s value %d \n", thirdObj->key,
+           thirdObj->value);
   }
 
   return 1;
@@ -51,16 +71,33 @@ unsigned int calculate_hash(char *key) {
   return hash;
 }
 
-unsigned int put_in_JsonObject(JsonArray *array, char *key, char *value) {
+unsigned int put_in_JsonObject(JsonArray *array, char *key, char *value,
+                               JsonValueType valueType) {
   unsigned int index = calculate_hash(key) % array->size;
+  JsonObject obj;
   if (array->jsonObjects[index].valid == 1) {
-    printf("ERROR: The cell its not empty!\n");
-  } else {
-    strcpy(array->jsonObjects[index].key, key);
-    strcpy(array->jsonObjects[index].value, value);
-    array->jsonObjects[index].valid = 1;
+    printf("ERROR: The cell its not empty! Rewriting the old with new one!\n");
+  }
+  switch (valueType) {
+  case JSON_VALUE_STRING: {
+    obj.valueType = JSON_VALUE_STRING;
+    obj.key = strdup(key);
+    obj.value.stringValue = strdup(value);
+    obj.valid = 1;
+    array->jsonObjects[index] = obj;
     return 1;
   }
+  case JSON_VALUE_INT: {
+
+    obj.valueType = JSON_VALUE_INT;
+    obj.key = strdup(key);
+    obj.valid = 1;
+    obj.value.intValue = atoi(value);
+    array->jsonObjects[index] = obj;
+    return 1;
+  }
+  }
+  return 0;
 }
 
 JsonObject *getObject(JsonArray *array, char *key) {
@@ -80,9 +117,10 @@ JsonArray parse_json(char *json, JsonArray *array) {
   int ending = 0;
   bool start = true;
   char *key = NULL;
-  bool valueStarting = false;
   for (int x = 0; x < strlen(json); x++) {
-    if (json[x] == '\"') {
+
+    switch (json[x]) {
+    case '\"': {
       // parsing string
       if (start) {
         starting = x + 1;
@@ -91,23 +129,46 @@ JsonArray parse_json(char *json, JsonArray *array) {
         ending = x - 1;
         start = true;
       }
-    }
-    if (json[x] == ':') {
+      break;
+    };
+    case ':': {
       int key_len = ending - starting;
       key = (char *)malloc(key_len + 1);
+
       strncpy(key, &json[starting], key_len + 1);
-      starting = 0;
       ending = 0;
-    }
-    if (json[x] == ',' || json[x] == '}') {
+      if (isdigit(json[x + 1])) {
+        starting = x + 1;
+      } else {
+        starting = 0;
+      }
+      break;
+    };
+    case ',':
+    case '}': {
+      JsonValueType valueType;
+      if (ending == 0 && isdigit(json[x - 1]) && starting != 0) {
+
+        valueType = JSON_VALUE_INT;
+        ending = x - 1;
+      }
+      if (json[starting] == 't' || json[starting] == 'T' ||
+          json[starting] == 'f' || json[starting] == 'F') {
+        valueType = JSON_VALUE_BOOLEAN;
+      }
+      if (isalpha(json[starting])) {
+        valueType = JSON_VALUE_STRING;
+      }
       int valueLength = ending - starting;
       value = (char *)malloc(valueLength + 1);
       strncpy(value, &json[starting], valueLength + 1);
-      put_in_JsonObject(array, key, value);
+      put_in_JsonObject(array, key, value, valueType);
       starting = 0;
       ending = 0;
       free(key);
       free(value);
+      break;
+    };
     }
   }
 }
